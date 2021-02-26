3c77ad49a181f590b993f88d7697b7cc3d11edf6
Charly Breadberry <aleks_inter@mail.ru>
2021-02-26 19:10:30 +0300

    fix edit form

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,10 +1,3 @@
-$(async function () {
-    await getTableWithUsers();
-    getNewUserForm();
-    getDefaultModal();
-    addNewUser();
-})
-
 
 const userFetchService = {
     head: {
@@ -14,11 +7,16 @@ const userFetchService = {
     },
     // bodyAdd : async function(user) {return {'method': 'POST', 'headers': this.head, 'body': user}},
     findAllUsers: () => fetch("/api/admin", {method: 'GET', headers: userFetchService.head,  }),
-    findOneUser: (id) => ({id: 1, firstname: "Peter", lastname: "Parker", age: 21, email: "spidey@evilcrop.com", roles: ["hero", "admin"]}),
+    findOneUser: (id) => fetch(`api/admin/user/${id}`),
     addNewUser: (user) => fetch('api/users', {method: 'POST', headers: userFetchService.head, body: JSON.stringify(user)}),
     updateUser: (user, id) => fetch(`api/users/${id}`, {method: 'PUT', headers: userFetchService.head, body: JSON.stringify(user)}),
     deleteUser: (id) => fetch(`api/users/${id}`, {method: 'DELETE', headers: userFetchService.head})
 }
+getTableWithUsers().then(_ => {
+    getNewUserForm();
+    getDefaultModal();
+    addNewUser();
+})
 
 async function getTableWithUsers() {
     let table = $('#mainTableWithUsers tbody');
@@ -35,14 +33,14 @@ async function getTableWithUsers() {
                             <td>${user.lastname}</td>
                             <td>${user.age}</td>
                             <td>${user.email}</td>
-                            <td>${user.roles.map(role => role.role).join(",")}</td>
+                            <td>${user.roles.map(role=>role.role).join(',')}</td>
                             <td>
                                 <button type="button" data-userid="${user.id}" data-action="edit" class="btn btn-outline-secondary"
-                                data-toggle="modal" data-target="#someDefaultModal"></button>
+                                data-toggle="modal" data-target="#userModal"></button>
                             </td>
                             <td>
                                 <button type="button" data-userid="${user.id}" data-action="delete" class="btn btn-outline-danger"
-                                data-toggle="modal" data-target="#someDefaultModal"></button>
+                                data-toggle="modal" data-target="#userModal"></button>
                             </td>
                         </tr>
                 )`;
@@ -53,7 +51,7 @@ async function getTableWithUsers() {
     // обрабатываем нажатие на любую из кнопок edit или delete
     // достаем из нее данные и отдаем модалке, которую к тому же открываем
     $("#mainTableWithUsers").find('button').on('click', (event) => {
-        let defaultModal = $('#someDefaultModal');
+        let defaultModal = $('#userModal');
 
         let targetButton = $(event.target);
         let buttonUserId = targetButton.attr('data-userid');
@@ -62,6 +60,7 @@ async function getTableWithUsers() {
         defaultModal.attr('data-userid', buttonUserId);
         defaultModal.attr('data-action', buttonAction);
         defaultModal.modal('show');
+        // editUser(defaultModal, buttonUserId);
     })
 }
 
@@ -86,7 +85,7 @@ async function getNewUserForm() {
 // что то деалем при открытии модалки и при закрытии
 // основываясь на ее дата атрибутах
 async function getDefaultModal() {
-    $('#someDefaultModal').modal({
+    $('#userModal').modal({
         keyboard: true,
         backdrop: "static",
         show: false
@@ -114,7 +113,7 @@ async function getDefaultModal() {
 // редактируем юзера из модалки редактирования, забираем данные, отправляем
 async function editUser(modal, id) {
     let preuser = await userFetchService.findOneUser(id);
-    let user = preuser.json();
+    let user = await preuser.json();
 
     modal.find('.modal-title').html('Edit user');
 
@@ -123,17 +122,22 @@ async function editUser(modal, id) {
     modal.find('.modal-footer').append(editButton);
     modal.find('.modal-footer').append(closeButton);
 
-    user.then(user => {
-        let bodyForm = `
-            <form class="form-group" id="editUser">
-                <input type="text" class="form-control" id="id" name="id" value="${user.id}" disabled><br>
-                <input class="form-control" type="text" id="login" value="${user.login}"><br>
-                <input class="form-control" type="password" id="password"><br>
-                <input class="form-control" id="age" type="number" value="${user.age}">
-            </form>
-        `;
-        modal.find('.modal-body').append(bodyForm);
-    })
+    let bodyForm = `
+        <form class="form-group" id="editUser">
+            <label class="font-weight-bold" >ID</label>
+            <input type="text" class="form-control" name="id" value="${user.id}" disabled><br>
+            <input class="form-control" id="AddNewUserFirstname" type="text" placeholder="first name" name="firstname" value="${user.firstname}"><br>
+            <input class="form-control" id="AddNewUserLastname" type="text" placeholder="last name" name="lastname" value="${user.lastname}"><br>
+            <input class="form-control" id="AddNewUserAge" type="number" placeholder="age" value="${user.age}"><br>
+            <input class="form-control" id="AddNewUserEmail" type="text" placeholder="email" name="email"  value="${user.email}"><br>
+            
+            <select  value="${user.roles}" multiple size="2" class="form-control"  id="editRoles" name="roles">
+                     <option value="ROLE_USER" selected="${user.roles.includes('ROLE_USER')}"> USER </option>
+                     <option value="ROLE_ADMIN" selected="${user.roles.includes('ROLE_ADMIN')}"> ADMIN </option>
+            </select>
+        </form>
+    `
+    modal.find('.modal-body').append(bodyForm);
 
     $("#editButton").on('click', async () => {
         let id = modal.find("#id").val().trim();
